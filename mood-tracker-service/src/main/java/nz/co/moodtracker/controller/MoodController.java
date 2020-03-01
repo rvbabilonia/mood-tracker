72a804df1d0f2c18a833e8eaad62c89ada095870
Rey Vincent Babilonia <rvbabilonia@gmail.com>
2020-03-01 21:34:38 +1300

    Added CORS and disabled SQL logging.

--- mood-tracker-service/src/main/java/nz/co/moodtracker/controller/MoodController.java
+++ mood-tracker-service/src/main/java/nz/co/moodtracker/controller/MoodController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping(value = "api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(origins = {"http://localhost:4200", "http://web:4200"}, allowedHeaders = "*")
 public class MoodController {
 
     private static final int MAX_AGE = 24 * 60 * 60; // expires in 24 hours
